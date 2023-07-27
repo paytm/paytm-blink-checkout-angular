4a0fe788bbfcc845907b3de7a256c981a75b4d86
Rahul Bisht <rahul2.bisht@paytm.com>
2023-07-27 13:18:15 +0530

    updated sample project

---
+++ merchant-example/src/app/merchant-config.ts
@@ -0,0 +1,34 @@
+export const CONFIG = {
+    flow: "DEFAULT",
+    merchant: {
+        mid: "INTEGR77698636129383",
+        name: "Nitin Khan D'souza",
+        logo: "",
+        redirect: true
+    },
+    style: {},
+    jsFile: "",
+    data: {
+        orderId: "integration_20230727123933",
+        amount: "1",
+        token: "c319380da4b74535b9bb56fa7152918b1690441774945",
+        tokenType: "TXN_TOKEN",
+        userDetail: {
+            mobileNumber: "",
+            name: ""
+        }
+    },
+    mapClientMessage: {},
+    labels: {},
+    payMode: {
+        labels: {},
+        filter: {
+            exclude: []
+        },
+        order: [
+            "NB",
+            "CARD",
+            "LOGIN"
+        ]
+    }
+};
